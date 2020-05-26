a38190215e4ac8a7ae85d4fde1cdf029829e2573
Yuan Jun <yuanjun221@gmail.com>
2020-05-26 21:43:35 +0800

    add animation

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -10,22 +10,71 @@ public class NBody {
 		String filename = args[2];
 		Body[] bodies = readBodies(filename);
 		double radius = readRadius(filename);
+		int len = bodies.length;
 
-		//StdDraw.enableDoubleBuffering();
+		StdDraw.enableDoubleBuffering();
 		StdDraw.setScale(-radius, radius);
-		StdDraw.clear();
-		StdDraw.picture(0, 0, backgroundImage);
 
-		for (Body b : bodies) {
-			b.draw();
+		/**
+		 * For each time through the loop
+		 */
+		for (double time = 0; time <= T; time += dt) {
+			/**
+			 * Calculate the net x and y forces for each Body, storing these
+			 * in the xForces and yForces arrays respectively.
+			 */
+			double xForces[] = new double[len];
+			double yForces[] = new double[len];
+			for (int i = 0; i < len; i++) {
+				xForces[i] = bodies[i].calcNetForceExertedByX(bodies);
+				yForces[i] = bodies[i].calcNetForceExertedByY(bodies);
+			}
+
+			/**
+			 * Update each body's position, velocity and acceleration
+			 * after calculating the net forces for every Body.
+			 */
+			for (int i = 0; i < len; i++) {
+				bodies[i].update(dt, xForces[i], yForces[i]);
+			}
+
+			/**
+			 * Draw the background image.
+			 */
+			StdDraw.clear();
+			StdDraw.picture(0, 0, backgroundImage);
+
+			/**
+			 * Draw all of the Bodys.
+			 */
+			for (Body b : bodies) {
+				b.draw();
+			}
+
+			/**
+			 * Show the offscreen buffer.
+			 */
+			StdDraw.show();
+			/**
+			 * Pause the animation for 10 milliseconds.
+			 */
+			StdDraw.pause(10);
 		}
 
-		//StdDraw.show();
-		//StdDraw.pause(2000);
+		/**
+		 * Print the Universe
+		 */
+		StdOut.printf("%d\n", bodies.length);
+		StdOut.printf("%.2e\n", radius);
+		for (int i = 0; i < bodies.length; i++) {
+    		StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+            bodies[i].xxPos, bodies[i].yyPos, bodies[i].xxVel,
+            bodies[i].yyVel, bodies[i].mass, bodies[i].imgFileName);   
+		}
 	}
 
 	/**
-	 * Read radius from a given file
+	 * Read radius from a given file.
 	 * @param fileName		a string represent the directory with the file name
 	 * @return				the raduis read from the file
 	 */
@@ -36,7 +85,7 @@ public class NBody {
 	}
 
 	/**
-	 * Create a array which holds Body instances read from a given file
+	 * Create a array which holds Body instances read from a given file.
 	 * @param fileName		a string represent the directory with the file name
 	 * @return				a array holds Body instances
 	 */
