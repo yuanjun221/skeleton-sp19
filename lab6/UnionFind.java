ddb03aeb5571ac135456ec683840a03c9dc9bfb2
Yuan Jun <yuanjun221@gmail.com>
2020-08-11 12:31:42 +0800

    complete lab6

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,35 +1,51 @@
 public class UnionFind {
 
-    // TODO - Add instance variables?
+    int[] id;
+    int[] sz;
+    int count;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        count = n;
+
+        id = new int[n];
+        sz = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        if (vertex < 0 || vertex >= id.length) {
+            throw new IllegalArgumentException("Invalid index");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        int root = find(v1);
+        return sz[root];
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        if (v1 == find(v1)) {
+            return -sz[v1];
+        }
+        return id[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
+        validate(v1);
+        validate(v2);
+        return find(v1) == find(v2);
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -38,14 +54,42 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        validate(v1);
+        validate(v2);
+
+        if (connected(v1, v2)) {
+            return;
+        }
+
+        int r1 = find(v1);
+        int r2 = find(v2);
+
+        if (sz[r1] <= sz[r2]) {
+            id[r1] = r2;
+            sz[r2] += sz[r1];
+        } else {
+            id[r2] = r1;
+            sz[r1] += sz[r2];
+        }
+        count--;
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
-        return -1;
+        validate(vertex);
+        int root = vertex;
+        while (root != id[root]) {
+            root = id[root];
+        }
+
+        // path compression
+        while (id[vertex] != root) {
+            id[vertex] = root;
+            vertex = id[vertex];
+        }
+
+        return root;
     }
 
 }
